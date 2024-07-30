587deb5434f4c0356854c8ad08a24df5338c8529
Matheus Oliveira <matheus.oliveira110101@gmail.com>
2024-07-30 12:00:00 -0300

    feat: make auth store

---
+++ src/stores/auth.ts
@@ -0,0 +1,15 @@
+import { defineStore } from 'pinia';
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    user: null
+  }),
+  getters: {
+    isAuthenticated: (state) => !!state?.user
+  },
+  actions: {
+    
+  }
+});
+
+export default useAuthStore
